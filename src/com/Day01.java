849edeb6f3fb5aa6d42364baa1f53f30d302e8b6
godcqaq <2089789425@qq.com>
2022-03-29 12:02:32 +0800

    测试gitignore插件，完成初始化

--- src/com/Day01.java
+++ src/com/Day01.java
@@ -3,5 +3,6 @@ package com;
 public abstract class Day01 {
     public static void main(String[] args) {
         System.out.println("hello world,hello git");
+        System.out.println("测试gitignore插件，完成初始化");
     }
 }
